fccf5ce8f4a4355a2888180db086906ad09150b2
LindaAhmeti <93882633+LindaAhmeti@users.noreply.github.com>
2023-04-15 17:32:45 +0200

    Feature: Searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,32 @@
 import './assets/styles/App.css';
 import Navbar from "./component/Navbar";
 import SearchBar from './component/SearchBar';
+import Footer from './component/Footer';
+
+import { ScreenClassProvider } from 'react-grid-system';
+
+
 function App() {
   return ( 
-  
-  <div className="App">
-   <div className='navbar'>
-    <Navbar/>
-    <SearchBar/>
-   </div>
+    
+    <ScreenClassProvider className='App'>
 
+    <Navbar className='navbar'/>
+   <div className='searchbar--'>
+    <SearchBar />
    </div>
+ 
+
+ {/* <Footer/> */}
+</ScreenClassProvider>
+  
+
   );
 }
 
 export default App;
+
+
 
 
 
--- client/src/component/SearchBar.js
+++ client/src/component/SearchBar.js
@@ -1,12 +1,41 @@
-import React from "react";
-
+import React,{useState} from "react";
+import '../assets/styles/sbar.css';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import DirectionsIcon from '@mui/icons-material/Directions';
 
 
 export default function SearchBar(){
     return(
-        <div>
+       <div className="firstpart" >
+        <div className="text--">
+       <b>Porosi për delivery</b> në Prishtinë
+      <br/><span className="span">Porosit ushqim online tani!</span>
+        </div>
+
+
+        <Paper className="searchbar"
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Qyteti,Komuna"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+    </Paper>
+
+
+    </div>
+
 
          
-        </div>
+        
     )
 }
