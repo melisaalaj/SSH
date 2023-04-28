88ee69335f46310cc81e664f2f8431f603047f18
Jeta-Kajtazi <94572246+Jeta-Kajtazi@users.noreply.github.com>
2023-04-28 20:35:36 +0200

    feature: creating signup page

--- client/src/pages/LoginSignUp.js
+++ client/src/pages/LoginSignUp.js
@@ -1,7 +1,47 @@
 import React from "react";
 
+import "./LoginSignUp.css"
 
 
-export default function LoginSignUp(){
-   
+const LoginSignUp =()=>{
+    return (
+      
+	
+	<div class="card">
+		<div class="card-image">	
+			<h2 class="card-heading">
+				Get started
+				<small>Let us create your account</small>
+			</h2>
+		</div>
+		<form class="card-form">
+			<div class="input">
+				<input type="text" class="input-field" required/>
+				<label class="input-label">Full name</label>
+			</div>
+						<div class="input">
+				<input type="text" class="input-field"  required/>
+				<label class="input-label">Email</label>
+			</div>
+						<div class="input">
+				<input type="password" class="input-field" required/>
+				<label class="input-label">Password</label>
+			</div>
+			<div class="action">
+				<button class="action-button">Get started</button>
+			</div>
+		</form>
+		<div class="card-info">
+			<p>Already have an account? <a href="#">Sign In</a></p>
+		</div>
+	
+</div>
+
+    )
 }
+
+export default LoginSignUp
+
+
+
+
