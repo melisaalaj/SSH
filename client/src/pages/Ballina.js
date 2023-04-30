6ad55cbd4d51ff7ceb893cbe8796ac728975d819
LindaAhmeti <93882633+LindaAhmeti@users.noreply.github.com>
2023-04-30 16:08:30 +0200

    feature:Update

--- client/src/pages/Ballina.js
+++ client/src/pages/Ballina.js
@@ -1,7 +1,24 @@
 import React from "react";
+import SearchBar from '../component/SearchBar';
+import Footer from '../component/Footer';
+import Carousel1 from '../component/Carousel1';
+import '../assets/styles/App.css';
 
+import { ScreenClassProvider } from 'react-grid-system';
 
+ 
 
 export default function Ballina(){
-   
+   return(
+    <ScreenClassProvider className='App'>
+
+   <div className='searchbar--'>
+    <SearchBar />
+   </div>
+ 
+   <Carousel1/>
+
+ {/* <Footer/> */}
+</ScreenClassProvider>
+   )
 }
