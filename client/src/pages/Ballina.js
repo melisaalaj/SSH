d8973634339b2e2c345d44f5555b762ebd5dcaea
Jeta-Kajtazi <jeta.kajtazi@student.uni-pr.edu>
2023-05-08 23:25:27 +0200

    Changes on homepage

--- client/src/pages/Ballina.js
+++ client/src/pages/Ballina.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "../component/Navbar";
+import SearchBar from "../component/SearchBar";
+import Carousel1 from "../component/Carousel1";
+import Footer from "../component/Footer";
 
-
-
-export default function Ballina(){
-   
+export default function Ballina() {
+  return (
+    <>
+      <Navbar className="navbar" />
+      <div className="searchbar--">
+        <SearchBar />
+      </div>
+      <Carousel1 />
+    </>
+  );
 }
