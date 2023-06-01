d0d234887f5225259234c92bda6c30d13e7cec45
Erza Lumi <erzalumi02@gmail.com>
2023-06-01 20:02:56 +0200

    Apply CSS styling in some components and pages

--- client/src/component/SearchBar.js
+++ client/src/component/SearchBar.js
@@ -14,19 +14,21 @@ export default function SearchBar() {
     event.preventDefault();
     if (location.trim() !== "") {
       try {
-        const locationParts = location.trim().split(","); 
+        const locationParts = location.trim().split(",");
         const city = locationParts[0].trim();
-        const street = locationParts[1].trim(); 
+        const street = locationParts[1].trim();
 
         // Make an API request to fetch the restaurant location
         const response = await fetch(
-          `http://localhost:3000/api/location?city=${encodeURIComponent(city)}&street=${encodeURIComponent(street)}`
+          `http://localhost:3000/api/location?city=${encodeURIComponent(
+            city
+          )}&street=${encodeURIComponent(street)}`
         );
         const data = await response.json();
         if (data.restaurants.length > 0) {
           // Get the ID of the first restaurant in the list
           const restaurantId = data.restaurants[0].id;
-  
+
           // Navigate to the restaurant page using the retrieved ID
           // navigate(`/restaurants/:${restaurantId}`);
           navigate(`/restaurants/:menuid`);
@@ -44,30 +46,64 @@ export default function SearchBar() {
   };
 
   return (
-    <div className="firstpart">
+    <div
+      className="firstpart"
+      style={{
+        textAlign: "center",
+        color: "#FFF",
+        fontFamily: "sans-serif",
+      }}
+    >
       <div className="text--">
-        <b>Order for delivery</b> in Prishtinë
+        <b>Order food for delivery in Prishtinë</b>
         <br />
-        <span className="span">Order food online now!</span>
+        <span className="span">Order online now!</span>
       </div>
 
-      <Paper  className="searchbar" component="form"
+      <Paper
+        className="searchbar"
+        component="form"
         sx={{
           p: "2px 4px",
           display: "flex",
           alignItems: "center",
           width: 400,
-        }}   onSubmit={handleSearch} >
+          backgroundColor: "#FFF",
+          borderRadius: "20px",
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
+        onSubmit={handleSearch}
+      >
         <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Address: "
+          sx={{
+            ml: 1,
+            flex: 1,
+            fontSize: "16px",
+            padding: "8px",
+            borderRadius: "20px",
+            border: "none",
+            outline: "none",
+          }}
+          placeholder="Address:"
           value={location}
           onChange={handleChange}
         />
-        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search" onChange={handleSearch}>
-          <SearchIcon  />
+        <IconButton
+          type="submit"
+          sx={{
+            p: "10px",
+            backgroundColor: "#e01818",
+            borderRadius: "50%",
+            transition: "background-color 0.3s ease",
+            "&:hover": {
+              backgroundColor: "#831a16",
+            },
+          }}
+          aria-label="search"
+          onChange={handleSearch}
+        >
+          <SearchIcon style={{ color: "#FFF" }} />
         </IconButton>
-       
       </Paper>
     </div>
   );
