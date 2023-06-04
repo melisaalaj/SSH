c40e86ea776b7505fc357f238b2f25613b043843
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2023-06-04 16:47:49 +0200

    feature: lolcation

--- client/src/component/SearchBar.js
+++ client/src/component/SearchBar.js
@@ -9,34 +9,34 @@ import SearchIcon from "@mui/icons-material/Search";
 export default function SearchBar() {
   const navigate = useNavigate();
   const [location, setLocation] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
 
   const handleSearch = async (event) => {
     event.preventDefault();
     if (location.trim() !== "") {
       try {
-        const locationParts = location.trim().split(",");
-        const city = locationParts[0].trim();
-        const street = locationParts[1].trim();
-
-        // Make an API request to fetch the restaurant location
-        const response = await fetch(
-          `http://localhost:3000/api/location?city=${encodeURIComponent(
-            city
-          )}&street=${encodeURIComponent(street)}`
+        const city = location.trim();
+        const token = localStorage.getItem("accessToken");
+        const response = await fetch(`http://localhost:3000/api/restaurants/getByCity`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ city }),
+          }
         );
         const data = await response.json();
-        if (data.restaurants.length > 0) {
-          // Get the ID of the first restaurant in the list
-          const restaurantId = data.restaurants[0].id;
-
-          // Navigate to the restaurant page using the retrieved ID
-          // navigate(`/restaurants/:${restaurantId}`);
-          navigate(`/restaurants/:menuid`);
+        console.log(data);
+        if (data.length > 0) {
+          const restaurantId = data[0].id; // Assuming you want to navigate to the first restaurant found
+          navigate(`/restaurant/${restaurantId}?location=${city}`);
         } else {
-          console.log("No restaurants found for the given location.");
+          console.log("No restaurants found for the given city.");
         }
       } catch (error) {
-        console.error("Error fetching location data:", error);
+        console.error("Error fetching restaurants data:", error);
       }
     }
   };
@@ -46,18 +46,11 @@ export default function SearchBar() {
   };
 
   return (
-    <div
-      className="firstpart"
-      style={{
-        textAlign: "center",
-        color: "#FFF",
-        fontFamily: "sans-serif",
-      }}
-    >
+    <div className="firstpart">
       <div className="text--">
-        <b>Order food for delivery in Prishtinë</b>
+        <b>Order for delivery</b> in Prishtinë
         <br />
-        <span className="span">Order online now!</span>
+        <span className="span">Order food online now!</span>
       </div>
 
       <Paper
@@ -68,41 +61,17 @@ export default function SearchBar() {
           display: "flex",
           alignItems: "center",
           width: 400,
-          backgroundColor: "#FFF",
-          borderRadius: "20px",
-          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
         }}
         onSubmit={handleSearch}
       >
         <InputBase
-          sx={{
-            ml: 1,
-            flex: 1,
-            fontSize: "16px",
-            padding: "8px",
-            borderRadius: "20px",
-            border: "none",
-            outline: "none",
-          }}
-          placeholder="Address:"
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="City: "
           value={location}
           onChange={handleChange}
         />
-        <IconButton
-          type="submit"
-          sx={{
-            p: "10px",
-            backgroundColor: "#e01818",
-            borderRadius: "50%",
-            transition: "background-color 0.3s ease",
-            "&:hover": {
-              backgroundColor: "#831a16",
-            },
-          }}
-          aria-label="search"
-          onChange={handleSearch}
-        >
-          <SearchIcon style={{ color: "#FFF" }} />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
         </IconButton>
       </Paper>
     </div>
